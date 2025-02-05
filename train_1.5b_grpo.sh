fd89f0b430bc868c8f62d38c20ebb64c14a8a34a
Ismail Emir Ayvaz <83560011+ieayvaz@users.noreply.github.com>
2025-02-05 19:16:01 +0300

    Update train_1.5b_grpo.sh

--- train_1.5b_grpo.sh
+++ train_1.5b_grpo.sh
@@ -1,10 +1,10 @@
 export N_GPUS=2
 export CUDA_VISIBLE_DEVICES=2,3
 ray stop --force && ray start --head --include-dashboard=True
-export BASE_MODEL="model/Qwen2.5-1.5B"
+export BASE_MODEL="model/Qwen2.5-1.5b"
 export DATA_DIR="data/countdown"
 export ROLLOUT_TP_SIZE=2
-export EXPERIMENT_NAME=countdown-qwen2.5-3b-grpo
+export EXPERIMENT_NAME=countdown-qwen2.5-1.5b-grpo
 export VLLM_ATTENTION_BACKEND=XFORMERS
 
 bash ./scripts/train_tiny_zero_a100_grpo.sh
